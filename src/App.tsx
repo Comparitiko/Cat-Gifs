4c8e4941c92468d2927e88b0139594a1644666ab
Comparitiko <gabigcy@gmail.com>
2023-12-30 20:24:31 +0100

    Prueba tecnica finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import {type fact} from './types/api'
+import './styles/app.css'
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  const [fact, setFact] = useState('')
+  const [urlImage, setUrlImage] = useState('')
+  const [search, setSearch] = useState(true)
+
+  const buscar = () => {
+    setSearch(true)
+  }
+
+  useEffect(() => {
+    fetch('https://catfact.ninja/fact')
+      .then((res) => res.json())
+      .then((data: fact) => {
+        setFact(data.fact)
+        setUrlImage(`https://cataas.com/cat/says/${data.fact.split(' ', 1)}`)
+        setSearch(false)
+      })
+  }, [search])
 
   return (
     <>
-      <section className=''>
-        <h1>asd</h1>
+      <section>
+        <h3>{fact}</h3>
+        <img src={urlImage} alt={`A cat with text ${fact}`} />
+        <button onClick={buscar}>Otra foto</button>
       </section>
     </>
   )
---
+++ src/types/api.ts
@@ -0,0 +1,3 @@
+export interface fact {
+  fact: string
+}
